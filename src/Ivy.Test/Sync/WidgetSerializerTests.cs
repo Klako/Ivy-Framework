564fd498a7178b2c33f3700612899860fdcae087
klako <4390016+Klako@users.noreply.github.com>
2026-03-20 13:23:19 +0100

    Added/edited tests

--- src/Ivy.Test/Sync/WidgetSerializerTests.cs
+++ src/Ivy.Test/Sync/WidgetSerializerTests.cs
@@ -5,12 +5,13 @@ using System.Text;
 using Ivy.Core.Sync;
 using MessagePack;
 using MessagePack.Resolvers;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
 namespace Ivy.Test.Sync
 {
     public class WidgetSerializerTests
     {
-        private record TestWidget : WidgetBase<TestWidget>
+        public record TestWidget : WidgetBase<TestWidget>
         {
             [Prop]
             public string TestProp1 { get; set; } = "default";
@@ -19,8 +20,52 @@ namespace Ivy.Test.Sync
             public EventHandler<Event<TestWidget>>? TestEvent { get; set; }
         }
 
+        [MessagePackObject]
+        public record ExpectedDeserializedStructure
+        {
+            [Key(0)]
+            public required string Type { get; init; }
+
+            [Key(1)]
+            public required string Id { get; init; }
+
+            [Key(2)]
+            public required IDictionary<string, object> Props { get; init; }
+
+            [Key(3)]
+            public required string[] Events { get; init; }
+
+            [Key(4)]
+            public required ExpectedDeserializedStructure[] Children { get; init; }
+        }
+
+        private static MessagePackSerializerOptions _serializerOptions =
+            new MessagePackSerializerOptions(CompositeResolver.Create([new WidgetSerializer()], [StandardResolver.Instance]));
+
         [Fact]
-        public void TestWidget_Serializes_Values()
+        public void TestWidget_BasicValues()
+        {
+            var expectedType = "Ivy.Test.Sync.TestWidget";
+            var expectedId = "greoij";
+            var expectedEvents = new string[0];
+
+            var widget = new TestWidget()
+            {
+                Id = expectedId
+            };
+
+            var data = MessagePackSerializer.Serialize<Core.IWidget>(widget, _serializerOptions);
+            var result = MessagePackSerializer.Deserialize<ExpectedDeserializedStructure>(data, _serializerOptions);
+
+            Assert.Equal(expectedType, result.Type);
+            Assert.Equal(expectedId, result.Id);
+            Assert.Empty(result.Props);
+            Assert.Empty(result.Events);
+            Assert.Empty(result.Children);
+        }
+
+        [Fact]
+        public void TestWidget_AddedValues()
         {
             var expectedType = "Ivy.Test.Sync.TestWidget";
             var expectedId = "doijqew";
@@ -34,27 +79,47 @@ namespace Ivy.Test.Sync
             };
             widget.Id = expectedId;
 
-            var resolver = CompositeResolver.Create([new WidgetSerializer()], [StandardResolver.Instance]);
+            var data = MessagePackSerializer.Serialize<Core.IWidget>(widget, _serializerOptions);
+            var obj = MessagePackSerializer.Deserialize<ExpectedDeserializedStructure>(data, _serializerOptions);
 
-            var serializeOptions = new MessagePackSerializerOptions(resolver);
+            Assert.Equal(expectedType, obj.Type);
+            Assert.Equal(expectedId, obj.Id);
+            Assert.Equal(expectedEvents, obj.Events);
+            Assert.NotEmpty(obj.Props);
+            var objProp1 = obj.Props["testProp1"];
+            Assert.Equal(expectedProp1, objProp1);
+            Assert.Empty(obj.Children);
+        }
 
-            var data = MessagePackSerializer.Serialize<Ivy.Core.IWidget>(widget, serializeOptions);
+        [Fact]
+        public void TestWidget_WithChildren()
+        {
+            var expectedId = "greoij";
+            var expectedChild1Id = "diojwef";
+            var expectedChild2Id = "fgewoijf";
+            var expectedEvents = new string[0];
 
-            var deserializeOptions = new MessagePackSerializerOptions(StandardResolver.Instance);
+            var widget = new TestWidget()
+            {
+                Id = expectedId,
+                Children = new object[]
+                {
+                    new TestWidget(){Id = expectedChild1Id, TestProp1 = "nondefault1"},
+                    new TestWidget(){Id = expectedChild2Id}
+                }
+            };
 
-            var obj = MessagePackSerializer.Deserialize<(string type,
-                string id,
-                IDictionary<string, object> props,
-                string[] events,
-                object[] children)>(data, deserializeOptions);
+            var data = MessagePackSerializer.Serialize<Core.IWidget>(widget, _serializerOptions);
+            var result = MessagePackSerializer.Deserialize<ExpectedDeserializedStructure>(data, _serializerOptions);
 
-            Assert.Equal(obj.type, expectedType);
-            Assert.Equal(obj.id, expectedId);
-            Assert.Equal(obj.events, expectedEvents);
-            Assert.NotEmpty(obj.props);
-            var objProp1 = obj.props["testProp1"];
-            Assert.Equal(objProp1, expectedProp1);
-            Assert.Empty(obj.children);
+            Assert.Equal(2, result.Children.Length);
+            var child1 = result.Children[0];
+            Assert.Equal(expectedChild1Id, child1.Id);
+            Assert.Contains("testProp1", child1.Props);
+            Assert.Equal("nondefault1", child1.Props["testProp1"]);
+            var child2 = result.Children[1];
+            Assert.Equal(expectedChild2Id, child2.Id);
+            Assert.DoesNotContain("testProp1", child2.Props);
         }
     }
 }

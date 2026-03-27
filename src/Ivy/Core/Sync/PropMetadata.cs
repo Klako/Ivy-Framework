5a440eb89fc8ad3ddd3df418d2401811e0194238
klako <4390016+Klako@users.noreply.github.com>
2026-03-27 16:19:20 +0100

    Separated widget metadata from usage of it.

---
+++ src/Ivy/Core/Sync/PropMetadata.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using static Ivy.Core.Sync.WidgetMetadata;
+
+namespace Ivy.Core.Sync
+{
+    internal record PropMetadata
+    {
+        public bool AlwaysSerialize { get; }
+
+        public string CamelCaseName { get; }
+
+        public object? DefaultValue { get; }
+
+        private PropertyInfo propInfo;
+
+        private PropAttribute attribute;
+
+        public PropMetadata(PropertyInfo propInfo, PropAttribute attribute, object? defaultValue)
+        {
+            AlwaysSerialize = attribute.AlwaysSerialize;
+            CamelCaseName = Utils.PascalCaseToCamelCase(propInfo.Name);
+            DefaultValue = defaultValue;
+            this.propInfo = propInfo;
+            this.attribute = attribute;
+        }
+
+        public object? GetValue(IWidget widget)
+        {
+            if (attribute.IsAttached)
+            {
+                if (!propInfo.PropertyType.IsArray || !propInfo.PropertyType.GetElementType()!.IsGenericType)
+                    throw new InvalidOperationException("Attached properties must be arrays of nullable types.");
+
+                var children = widget.Children;
+                var attachedValues = new object?[children.Length];
+                var widgetType = widget.GetType();
+                var attachedName = attribute.AttachedName!;
+
+                for (int i = 0; i < children.Length; i++)
+                {
+                    if (children[i] is IWidget childWidget)
+                    {
+                        attachedValues[i] = childWidget.GetAttachedValue(widgetType, attachedName);
+                    }
+                }
+                return attachedValues;
+            }
+
+            return propInfo.GetValue(widget);
+        }
+    }
+}
---
+++ src/Ivy/Core/Sync/WidgetMetadata.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Ivy.Core.Sync
+{
+    internal record WidgetMetadata
+    {
+
+        private PropertyInfo[] eventProperties;
+        
+        public string TypeName { get; }       
+
+        public IDictionary<string, PropMetadata> PropMetadatas { get; }
+
+        private WidgetMetadata(Type widgetType)
+        {
+            TypeName = CleanTypeName(widgetType);
+
+            var allProperties = widgetType.GetProperties();
+
+            IWidget? defaultInstance = null;
+            var defaultCtor = widgetType.GetConstructor(
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic,
+                null,
+                Type.EmptyTypes,
+                null);
+
+            if (defaultCtor != null)
+            {
+                try
+                {
+                    defaultInstance = defaultCtor.Invoke(null) as IWidget;
+                } catch
+                {
+                    // Ignore construction failures - we'll just not have default values
+                }
+            }
+
+            PropMetadatas = allProperties
+                .Select(p => (Property: p, Attribute: p.GetCustomAttribute<PropAttribute>()))
+                .Where(x => x.Attribute != null)
+                .Select(x => new PropMetadata(x.Property, x.Attribute!, x.Property.GetValue(defaultInstance)))
+                .ToDictionary(x => x.CamelCaseName);
+
+            eventProperties = allProperties
+                .Where(p => p.GetCustomAttribute<EventAttribute>() != null)
+                .ToArray();
+        }
+
+        public static string CleanTypeName(Type t)
+        {
+            return t.Namespace + "." + Utils.CleanGenericNotation(t.Name);
+        }
+
+        public IList<string> GetEvents(IWidget widget)
+        {
+            var events = new List<string>(eventProperties.Length);
+            foreach (var info in eventProperties)
+            {
+                if (info.GetValue(widget) != null)
+                {
+                    events.Add(info.Name);
+                }
+            }
+            return events;
+        }
+
+        private static ConcurrentDictionary<Type, WidgetMetadata> cache = new();
+
+        public static WidgetMetadata FromWidgetType(Type type)
+        {
+            return cache.GetOrAdd(type, new WidgetMetadata(type));
+        }
+    }
+}

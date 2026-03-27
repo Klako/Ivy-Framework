486e737c6abfd199238009cf07ad054a658c9cc8
klako <4390016+Klako@users.noreply.github.com>
2026-03-27 16:19:20 +0100

    Separated widget metadata from usage of it.

--- src/Ivy/Core/Sync/WidgetSerializer.cs
+++ src/Ivy/Core/Sync/WidgetSerializer.cs
@@ -10,89 +10,6 @@ namespace Ivy.Core.Sync
 {
     internal class WidgetSerializer : IMessagePackFormatter<IWidget>
     {
-        private static readonly ConcurrentDictionary<Type, SerializationTypeMetadata> MetadataCache = new();
-
-        private sealed record PropInfo(PropertyInfo Property, PropAttribute Attribute, string CamelCaseName);
-
-        private sealed record EventInfo(PropertyInfo Property);
-
-        private sealed record SerializationTypeMetadata(
-            string TypeName,
-            PropInfo[] PropProperties,
-            EventInfo[] EventProperties,
-            IWidget? DefaultInstance
-        );
-
-        public static string CleanTypeName(Type t)
-        {
-            return t.Namespace + "." + Utils.CleanGenericNotation(t.Name);
-        }
-        private static SerializationTypeMetadata GetMetadata(Type type)
-        {
-            return MetadataCache.GetOrAdd(type, static t =>
-            {
-                var allProperties = t.GetProperties();
-
-                var propProperties = allProperties
-                    .Select(p => (Property: p, Attribute: p.GetCustomAttribute<PropAttribute>()))
-                    .Where(x => x.Attribute != null)
-                    .Select(x => new PropInfo(x.Property, x.Attribute!, Utils.PascalCaseToCamelCase(x.Property.Name)))
-                    .ToArray();
-
-                var eventProperties = allProperties
-                    .Where(p => p.GetCustomAttribute<EventAttribute>() != null)
-                    .Select(p => new EventInfo(p))
-                    .ToArray();
-
-                IWidget? defaultInstance = null;
-                var defaultCtor = t.GetConstructor(
-                    BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic,
-                    null,
-                    Type.EmptyTypes,
-                    null);
-
-                if (defaultCtor != null)
-                {
-                    try
-                    {
-                        defaultInstance = defaultCtor.Invoke(null) as IWidget;
-                    } catch
-                    {
-                        // Ignore construction failures - we'll just not have default values
-                    }
-                }
-
-                var typeName = CleanTypeName(t);
-
-                return new SerializationTypeMetadata(typeName, propProperties, eventProperties, defaultInstance);
-            });
-        }
-
-        private static object? GetPropertyValue(IWidget widget, PropertyInfo property, PropAttribute attribute)
-        {
-            if (attribute.IsAttached)
-            {
-                if (!property.PropertyType.IsArray || !property.PropertyType.GetElementType()!.IsGenericType)
-                    throw new InvalidOperationException("Attached properties must be arrays of nullable types.");
-
-                var children = widget.Children;
-                var attachedValues = new object?[children.Length];
-                var widgetType = widget.GetType();
-                var attachedName = attribute.AttachedName!;
-
-                for (int i = 0; i < children.Length; i++)
-                {
-                    if (children[i] is IWidget childWidget)
-                    {
-                        attachedValues[i] = childWidget.GetAttachedValue(widgetType, attachedName);
-                    }
-                }
-                return attachedValues;
-            }
-
-            return property.GetValue(widget);
-        }
-
         private static bool ValuesAreEqual(object? a, object? b)
         {
             if (Equals(a, b)) return true;
@@ -113,7 +30,7 @@ namespace Ivy.Core.Sync
 
         public void Serialize(ref MessagePackWriter writer, IWidget widget, MessagePackSerializerOptions options)
         {
-            var metadata = GetMetadata(widget.GetType());
+            var metadata = WidgetMetadata.FromWidgetType(widget.GetType());
 
             writer.WriteArrayHeader(5);
 
@@ -125,44 +42,34 @@ namespace Ivy.Core.Sync
 
             // Serialize props
 
-            var props = new List<(PropInfo, object)>(metadata.PropProperties.Length);
+            var props = new List<(string, object)>(metadata.PropMetadatas.Count);
 
-            foreach (var propInfo in metadata.PropProperties)
+            foreach (var (name, propMetadata) in metadata.PropMetadatas)
             {
-                var value = GetPropertyValue(widget, propInfo.Property, propInfo.Attribute);
+                var value = propMetadata.GetValue(widget);
 
                 // Skip properties that match their default values (unless AlwaysSerialize is set)
-                if (!propInfo.Attribute.AlwaysSerialize && metadata.DefaultInstance != null)
+                if (!propMetadata.AlwaysSerialize && propMetadata.DefaultValue != null)
                 {
-                    var defaultValue = GetPropertyValue(metadata.DefaultInstance, propInfo.Property, propInfo.Attribute);
-                    if (ValuesAreEqual(value, defaultValue))
+                    if (ValuesAreEqual(value, propMetadata.DefaultValue))
                         continue;
                 }
                 if (value != null)
                 {
-                    props.Add((propInfo, value));
+                    props.Add((name, value));
                 }
             }
 
             writer.WriteMapHeader(props.Count);
-            foreach (var (propInfo, propValue) in props)
+            foreach (var (propName, propValue) in props)
             {
-                writer.Write(propInfo.CamelCaseName);
+                writer.Write(propName);
                 MessagePackSerializer.Serialize(propValue.GetType(), ref writer, propValue, options);
             }
 
             // Serialize events
 
-            var events = new List<string>(metadata.EventProperties.Length);
-
-            foreach (var eventInfo in metadata.EventProperties)
-            {
-                var value = eventInfo.Property.GetValue(widget);
-                if (eventInfo.Property.GetValue(widget) != null)
-                {
-                    events.Add(eventInfo.Property.Name);
-                }
-            }
+            var events = metadata.GetEvents(widget);
 
             writer.WriteArrayHeader(events.Count);
             foreach (var e in events)

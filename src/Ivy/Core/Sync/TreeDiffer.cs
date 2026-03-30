389246863d4137b9a2f3e0de502f436780f4dce2
klako <4390016+Klako@users.noreply.github.com>
2026-03-30 16:04:02 +0200

    Added basic but incomplete difference and equality checks for a widget tree.

--- src/Ivy/Core/Sync/TreeDiffer.cs
+++ src/Ivy/Core/Sync/TreeDiffer.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using OpenAI.Chat;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Ivy.Core.Sync
@@ -13,14 +16,54 @@ namespace Ivy.Core.Sync
                 return target;
             }
 
-            string id = source.Id!;
+            string? id = null;
 
             if (id != target.Id)
             {
-                id = target.Id!;
+                id = target.Id;
             }
 
-            return null!;
+            // Widgets are the same type
+            var metadata = WidgetMetadata.FromWidgetType(source.GetType());
+
+            // Compare props
+
+            var propUpdates = new Dictionary<string, object?>();
+
+            foreach (var (name, propMetadata) in metadata.PropMetadatas)
+            {
+                var sourceValue = propMetadata.GetValue(source);
+                var targetValue = propMetadata.GetValue(target);
+
+                if (!StructuralComparisons.StructuralEqualityComparer.Equals(sourceValue, targetValue))
+                {
+                    propUpdates.Add(name, targetValue);
+                }
+            }
+
+            // Compare events
+
+            string[]? eventsUpdate = null;
+
+            var sourceEvents = metadata.GetEvents(source);
+            var targetEvents = metadata.GetEvents(target);
+
+            if (!StructuralComparisons.StructuralEqualityComparer.Equals(sourceEvents, targetEvents))
+            {
+                eventsUpdate = targetEvents.ToArray();
+            }
+
+            // Compare children
+
+            var childrenChanges = new List<IWidgetListOperation>();
+
+            
+
+            return new WidgetUpdate(
+                id: id,
+                props: propUpdates,
+                events: eventsUpdate,
+                children: new WidgetListDiff(changes: childrenChanges.ToArray()));
         }
     }
 }

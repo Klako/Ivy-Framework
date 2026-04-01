f082847c949b84850ab5395aa556e6cd6d03f82e
klako <4390016+Klako@users.noreply.github.com>
2026-04-01 15:17:25 +0200

    Added benchmark project

---
+++ src/Ivy.Benchmark.Sync/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Ivy.Benchmark.Sync
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var _ = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}

1f589fe019c38ecd22bd228081901d2271c396fe
klako <4390016+Klako@users.noreply.github.com>
2026-04-02 13:25:00 +0200

    Added benchmarks for 1000 element flat and binary trees. Included serialized size metric.

--- src/Ivy.Benchmark.Sync/WidgetTreeSync.cs
+++ src/Ivy.Benchmark.Sync/WidgetTreeSync.cs
@@ -1,28 +1,145 @@
 ﻿using BenchmarkDotNet.Attributes;
 using Microsoft.VSDiagnostics;
+using Ivy.Core;
+using Ivy.Core.Server.Formatters;
+using MessagePack;
+using MessagePack.Formatters;
+using MessagePack.Resolvers;
 using System;
+using System.Linq;
 using System.Security.Cryptography;
+using System.Text.Json.Nodes;
+using BenchmarkDotNet.ReportColumns;
+using Ivy.Core.Sync;
 
 namespace Ivy.Benchmark.Sync
 {
     // For more information on the VS BenchmarkDotNet Diagnosers see https://learn.microsoft.com/visualstudio/profiling/profiling-with-benchmark-dotnet
     [CPUUsageDiagnoser]
-    public class WidgetTreeSync
+    public partial class WidgetTreeSync
     {
-        private SHA256 sha256 = SHA256.Create();
-        private byte[] data;
+        IWidget flatTreeSource;
+        IWidget flatTreeTarget;
+
+        IWidget binaryTreeSource;
+        IWidget binaryTreeTarget;
+
+        MessagePackSerializerOptions _jsonNode2MsgPackOptions = MessagePackSerializerOptions.Standard.WithResolver(
+                CompositeResolver.Create(
+                    new IMessagePackFormatter[] {
+                        new JsonNodeMessagePackFormatter(),
+                        new JsonObjectMessagePackFormatter(),
+                        new JsonArrayMessagePackFormatter(),
+                        new JsonValueMessagePackFormatter()
+                    },
+                    new IFormatterResolver[] {
+                        JsonNodeResolver.Instance,
+                        ContractlessStandardResolver.Instance
+                    }
+                )
+            );
+
+        MessagePackSerializerOptions _newDiff2MsgPackOptions =
+            new MessagePackSerializerOptions(CompositeResolver.Create([new Core.Sync.WidgetSerializer()], [StandardResolver.Instance]));
+
+        private static string[] texts = [
+                "Five foxes four fairies",
+                "Lorem ipsum dolor sit amet",
+                "Aliquam augue massa",
+                "Vivamus lobortis diam id nulla mattis"
+            ];
+
+        private IWidget[] GenerateTexts(int n)
+        {
+            var widgets = new IWidget[n];
+            var rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                widgets[i] = (IWidget)Text.H3(texts[rand.Next(texts.Length)]).Build()!;
+                widgets[i].Id = i.ToString();
+            }
+            return widgets;
+        }
+
+        private IWidget GenerateBinaryTree(Random rand, int depth)
+        {
+            if (depth == 1)
+            {
+                return (IWidget)Text.H3(texts[rand.Next(texts.Length)]).Build()!; ;
+            }
+            var prefix = rand.Next().ToString();
+
+            var nodeText = (IWidget)Text.H3(texts[rand.Next(texts.Length)]).Build()!;
+            nodeText.Id = prefix + "1";
+            var left = GenerateBinaryTree(rand, depth - 1);
+            left.Id = prefix + "2";
+            var right = GenerateBinaryTree(rand, depth - 1);
+            right.Id = prefix + "3";
+            return new List(nodeText, left, right);
+        }
 
         [GlobalSetup]
         public void Setup()
         {
-            data = new byte[10000];
-            new Random(42).NextBytes(data);
+            flatTreeSource = new List(GenerateTexts(1000)) { Id = "pokqwd" };
+            flatTreeTarget = new List(GenerateTexts(1000)) { Id = "pokqwd" };
+            binaryTreeSource = GenerateBinaryTree(new Random(), (int)Math.Log2(1000));
+            binaryTreeSource.Id = "dwqpokqwd";
+            binaryTreeTarget = GenerateBinaryTree(new Random(), (int)Math.Log2(1000));
+            binaryTreeTarget.Id = "dwqpokqwd";
+        }
+
+        [ReportColumn(Aggregation = Aggregation.Mean, Name = "Size", Unit = "bytes")]
+        public int ResultSize { get; set; }
+
+        [Benchmark]
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+        public int RustDiff_FlatTree()
+        {
+            var sourceWidgetJson = flatTreeSource.Serialize();
+            var targetWidgetJson = flatTreeTarget.Serialize();
+            var oldBytes = System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(sourceWidgetJson);
+            var newBytes = System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(targetWidgetJson);
+            var patch = NativeJsonDiff.ComputePatch(oldBytes, newBytes);
+            var serialized = MessagePackSerializer.Serialize(patch, _jsonNode2MsgPackOptions);
+            ResultSize = serialized.Length;
+            return serialized.Length;
         }
 
         [Benchmark]
-        public byte[] Sha256()
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+        public int NewDiff_FlatTree()
         {
-            return sha256.ComputeHash(data);
+            var diff = Ivy.Core.Sync.TreeDiffer.ComputeDiff(flatTreeSource, flatTreeTarget);
+            var serialized = MessagePackSerializer.Serialize(diff, _newDiff2MsgPackOptions);
+            ResultSize = serialized.Length;
+            return serialized.Length;
         }
+
+
+        [Benchmark]
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+        public int RustDiff_BinaryTree()
+        {
+            var sourceWidgetJson = binaryTreeSource.Serialize();
+            var targetWidgetJson = binaryTreeTarget.Serialize();
+            var oldBytes = System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(sourceWidgetJson);
+            var newBytes = System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(targetWidgetJson);
+            var patch = NativeJsonDiff.ComputePatch(oldBytes, newBytes);
+            var serialized = MessagePackSerializer.Serialize(patch, _jsonNode2MsgPackOptions);
+            ResultSize = serialized.Length;
+            return serialized.Length;
+        }
+
+        [Benchmark]
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+        public int NewDiff_BinaryTree()
+        {
+            var diff = Ivy.Core.Sync.TreeDiffer.ComputeDiff(binaryTreeSource, binaryTreeTarget);
+            var serialized = MessagePackSerializer.Serialize(diff, _newDiff2MsgPackOptions);
+            ResultSize = serialized.Length;
+            return serialized.Length;
+        }
+
     }
 }

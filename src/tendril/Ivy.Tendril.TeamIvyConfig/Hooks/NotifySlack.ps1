2b32b247f22f93b7d557ba3ddd44ccd2a7f9aaad
nielsbosma <niels@bosmainteractive.se>
2026-04-03 23:54:01 +0200

    [01659] Fix Slack hook PR regex lookahead to stop at next YAML key

--- src/tendril/Ivy.Tendril.TeamIvyConfig/Hooks/NotifySlack.ps1
+++ src/tendril/Ivy.Tendril.TeamIvyConfig/Hooks/NotifySlack.ps1
@@ -35,7 +35,7 @@ if ($planContent -match "(?m)^project:\s*(.+)$") {
 
 # Parse PRs
 $prs = @()
-if ($planContent -match "(?ms)^prs:\s*\n((?:- .+\n?)*?)(?=\n\w+:|$)") {
+if ($planContent -match "(?m)^prs:\s*\n((?:- .+\n)*?)(?=\w+:|$)") {
     $prs = [regex]::Matches($Matches[1], "- (.+)") | ForEach-Object { $_.Groups[1].Value.Trim() }
 }
 

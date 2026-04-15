c3d7c2f94d2d1485ec324b2004043228d965c51c
Joel Bystedt <joel.bystedt@gmail.com>
2026-04-15 12:51:25 +0000

    [01515] Add bottom margin to tree item children container

--- src/frontend/src/widgets/tree/TreeItem.tsx
+++ src/frontend/src/widgets/tree/TreeItem.tsx
@@ -129,7 +129,7 @@ export const TreeItem: React.FC<TreeItemWidgetProps> = ({
         <CollapsibleContent className="overflow-hidden transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down">
           <div
             className={cn(
-              "ivy-tree-children flex flex-col pl-[1rem] ml-2 border-l border-border/50",
+              "ivy-tree-children flex flex-col pl-[1rem] ml-2 border-l border-border/50 mt-0.5",
               gapClass,
             )}
           >

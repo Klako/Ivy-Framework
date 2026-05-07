902da99c3622edd309bc87deaaec1958912f0002
klako <4390016+Klako@users.noreply.github.com>
2026-05-07 17:03:53 +0200

    Simplified tests with DRY

--- src/frontend/benchmark/simple.spec.ts
+++ src/frontend/benchmark/simple.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 import fs from 'fs';
 
 test.use({
@@ -6,6 +6,7 @@ test.use({
 });
 
 test.beforeEach(async ({ page }) => {
+  fs.existsSync('results/simple') || fs.mkdirSync('results/simple');
   await page.route('**/*', async (route) => {
     const response = await route.fetch();
     const headers = {
@@ -17,9 +18,8 @@ test.beforeEach(async ({ page }) => {
   });
 });
 
-test('writeText', async ({ page }) => {
+async function beforeBenchmark(page: Page, testname: string) {
   page.goto("https://localhost:5010/simple");
-  await page.waitForEvent('websocket'); // First websocket is app menu
   let ws = await page.waitForEvent('websocket');
   console.log(`WebSocket opened: ${ws.url()}`);
   let bytesRecieved = 0;
@@ -30,132 +30,103 @@ test('writeText', async ({ page }) => {
   });
   let latencies: number[] = [];
   let payloads: number[] = [];
-  let widgetUpdateBenchmarks : number[] = [];
+  let widgetUpdates: number[] = [];
 
-  await page.exposeFunction('recordWidgetTreeUpdateBenchmark', (parentId: string | null | undefined, benchmark : number) => {
-    if (parentId == new URL(ws.url()).searchParams.get('parentId')) {
-      widgetUpdateBenchmarks.push(benchmark);
-    }
+  await page.exposeFunction('recordWidgetTreeUpdateBenchmark', (parentId: string | null | undefined, benchmark: number) => {
+    widgetUpdates.push(benchmark);
   });
 
   let interactCounter = page.getByTestId('interactCounter');
   await expect(interactCounter).toBeVisible();
+
+  let previousInteractCounter = 0;
+  let previousPerformanceTime = performance.now();
+  let beforeInteract = () => {
+    previousPerformanceTime = performance.now();
+  }
+  let afterInteract = async () => {
+    await expect(interactCounter).toHaveAttribute('value', (previousInteractCounter + 1).toString());
+    previousInteractCounter++
+    payloads.push(bytesRecieved);
+    latencies.push(performance.now() - previousPerformanceTime);
+  }
+
+  let afterBenchmark = () => {
+    let f = fs.createWriteStream(`results/simple/${testname}.csv`);
+    f.write("Iteration,Payload,Latency,WidgetUpdate\n");
+    for (let [payload, latency, iteration, widgetUpdate] of payloads.map((p, i) => [p, latencies[i], i, widgetUpdates[i]])) {
+      f.write(`${iteration + 1},${payload},${latency},${widgetUpdate}\n`);
+    }
+    f.close();
+  }
+
+
+  return {
+    beforeInteract,
+    afterInteract,
+    afterBenchmark
+  };
+}
+
+test('write_text', async ({ page }) => {
+  let {
+    beforeInteract,
+    afterInteract,
+    afterBenchmark
+  } = await beforeBenchmark(page, 'write_text');
 
   let textToWrite = "The quick brown fox jumped over the lazy dog";
 
   let textInput = page.getByTestId("textInput");
 
-  let previousInteractCounter = 0;
-  let previousPerformanceTime = performance.now();
-  let nextInteractCount = async () => {
-    await expect(interactCounter).toHaveAttribute('value', (previousInteractCounter + 1).toString());
-    previousInteractCounter++
-    payloads.push(bytesRecieved);
-    latencies.push(performance.now() - previousPerformanceTime);
-    previousPerformanceTime = performance.now();
+  for (let i = 1; i < textToWrite.length; i++) {
+    beforeInteract();
+    await textInput.fill(textToWrite.slice(0, i));
+    await afterInteract();
   }
+  for (let i = 1; i < textToWrite.length; i++) {
+    beforeInteract();
+    await textInput.fill(textToWrite.slice(0, i));
+    await afterInteract();
+  }
+
+  afterBenchmark();
 });
 
-test('click checkbox', async ({ page }) => {
-  page.goto("https://localhost:5010/simple");
-  await page.waitForEvent('websocket'); // First websocket is app menu
-  let ws = await page.waitForEvent('websocket');
-  console.log(`WebSocket opened: ${ws.url()}`);
-  let bytesRecieved = 0;
-  ws.on('framereceived', event => {
-    if (event.payload.length > 20) {
-      bytesRecieved = event.payload.length;
-    }
-  });
-  let latencies: number[] = [];
-  let payloads: number[] = [];
-  let widgetUpdateBenchmarks : number[] = [];
+test('click_checkbox', async ({ page }) => {
+  let {
+    beforeInteract,
+    afterInteract,
+    afterBenchmark
+  } = await beforeBenchmark(page, 'click_checkbox');
 
-  await page.exposeFunction('recordWidgetTreeUpdateBenchmark', (parentId: string | null | undefined, benchmark : number) => {
-    if (parentId == new URL(ws.url()).searchParams.get('parentId')) {
-      widgetUpdateBenchmarks.push(benchmark);
-    }
-  });
-
-  let interactCounter = page.getByTestId('interactCounter');
-  await expect(interactCounter).toBeVisible();
-
-  let textInput = page.getByTestId("textInput");
   let boolInput = page.getByTestId("boolInput");
-  let radioInput = page.getByTestId("radioInput");
 
-  let textToWrite = "The quick brown fox jumped over the lazy dog";
-
-  let previousInteractCounter = 0;
-  let previousPerformanceTime = performance.now();
-  let nextInteractCount = async () => {
-    await expect(interactCounter).toHaveAttribute('value', (previousInteractCounter + 1).toString());
-    previousInteractCounter++
-    payloads.push(bytesRecieved);
-    latencies.push(performance.now() - previousPerformanceTime);
-    previousPerformanceTime = performance.now();
-  }
-  for (let i = 0; i < 200; i++) {
+  for (let i = 0; i < 100; i++) {
+    beforeInteract();
     await boolInput.click();
-    await nextInteractCount();
-  }
-  let currentRadioButton = 1;
-  for (let i = 0; i < 50; i++) {
-    await radioInput.getByRole("radio").nth(currentRadioButton).click();
-    currentRadioButton = (currentRadioButton + 1) % 3;
-    await nextInteractCount();
+    await afterInteract();
   }
 
-  fs.existsSync('results') || fs.mkdirSync('results');
-  let f = fs.createWriteStream(`results/simple-checkboxes.csv`);
-  f.write("Iteration,Payload,Latency,WidgetUpdate\n");
-  for (let [payload, latency, iteration, widgetUpdate] of payloads.map((p, i) => [p, latencies[i], i, widgetUpdateBenchmarks[i]])) {
-    f.write(`${iteration+1},${payload},${latency},${widgetUpdate}\n`);
-  }
-  f.close();
+  afterBenchmark();
 });
 
-test('click radiobuttons', async ({ page }) => {
-  page.goto("https://localhost:5010/simple");
-  await page.waitForEvent('websocket'); // First websocket is app menu
-  let ws = await page.waitForEvent('websocket');
-  console.log(`WebSocket opened: ${ws.url()}`);
-  let bytesRecieved = 0;
-  ws.on('framereceived', event => {
-    if (event.payload.length > 20) {
-      bytesRecieved = event.payload.length;
-    }
-  });
-  let latencies: number[] = [];
-  let payloads: number[] = [];
-  let widgetUpdateBenchmarks : number[] = [];
-
-  await page.exposeFunction('recordWidgetTreeUpdateBenchmark', (parentId: string | null | undefined, benchmark : number) => {
-    if (parentId == new URL(ws.url()).searchParams.get('parentId')) {
-      widgetUpdateBenchmarks.push(benchmark);
-    }
-  });
-  
-  
-  let interactCounter = page.getByTestId('interactCounter');
-  await expect(interactCounter).toBeVisible();
+test('click_radiobuttons', async ({ page }) => {
+  let {
+    beforeInteract,
+    afterInteract,
+    afterBenchmark
+  } = await beforeBenchmark(page, 'click_radiobuttons');
 
   let radioInput = page.getByTestId("radioInput");
 
-  let previousInteractCounter = 0;
-  let previousPerformanceTime = performance.now();
-  let nextInteractCount = async () => {
-    await expect(interactCounter).toHaveAttribute('value', (previousInteractCounter + 1).toString());
-    previousInteractCounter++
-    payloads.push(bytesRecieved);
-    latencies.push(performance.now() - previousPerformanceTime);
-    previousPerformanceTime = performance.now();
+  let currentRadioButton = 1;
+  for (let i = 0; i < 100; i++) {
+    beforeInteract();
+    await radioInput.getByRole("radio").nth(currentRadioButton).click();
+    await afterInteract();
+    currentRadioButton = (currentRadioButton + 1) % 3;
   }
 
-  let currentRadioButton = 1;
-  for (let i = 0; i < 50; i++) {
-    await radioInput.getByRole("radio").nth(currentRadioButton).click();
-    currentRadioButton = (currentRadioButton + 1) % 3;
-    await nextInteractCount();
-  }
+  afterBenchmark();
 })
